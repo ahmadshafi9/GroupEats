fdd00a0ef92f24f9182817cac7ce6d3674c4d92c
ahmad mogral <ahmad495836@gmail.com>
2026-01-28 23:11:12 -0800

    with added gitignores

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -11,13 +11,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5TchliypbjYP24aAPk50FgIIJv7__t5s",
-  authDomain: "groupeats-60549.firebaseapp.com",
-  projectId: "groupeats-60549",
-  storageBucket: "groupeats-60549.firebasestorage.app",
-  messagingSenderId: "412628177163",
-  appId: "1:412628177163:web:86a5b2e57531a2c6f8120e",
-  measurementId: "G-S0LB83G602"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
